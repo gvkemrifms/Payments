068fd79e14db9cac113ada4d93ea9e5b151ddcb4
vinod <narjun60@hotmail.com>
2018-05-11 13:07:45 +0530

    ChangePasswordUpdations

--- MainForm.aspx.cs
+++ MainForm.aspx.cs
@@ -1,48 +1,43 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace DailyCollectionAndPayments
 {
-    public partial class MainForm : System.Web.UI.Page
+    public partial class MainForm : Page
     {
-        private static readonly string CONN_STRING = ConfigurationManager.AppSettings["GvkEmriCon"];
-        DataTable dtMenuItems = new DataTable();
+        private static readonly string ConnString = ConfigurationManager.AppSettings["GvkEmriCon"];
+        readonly DataTable _dtMenuItems = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["RName"] == null)
-            {
                 Response.Redirect("Login.aspx");
-            }
 
             if (!IsPostBack)
             {
-                String strUserName = Session["RName"].ToString();
-                String strRoleid = Session["RoleId"].ToString();
+                var strUserName = Session["RName"].ToString();
+                var strRoleid = Session["RoleId"].ToString();
                 LblUserName.Text = strUserName;
-                MySqlCommand cmd = new MySqlCommand();
+                var cmd = new MySqlCommand();
                 string query = "SELECT mp.`previlage_id`, mp.`previlage_name`, `parent_previlage_id`, `path`  "
                         + "     FROM `finance`.`m_previlage` mp  "
                         + "     JOIN `finance`.`m_roleprevilage` ur ON mp.`previlage_id`= ur.`previlage_id`  "
                         + "     WHERE mp.`is_active`= 1 AND ur.role_id =  " + strRoleid + " ORDER BY order_by ASC; ";
-                MySqlDataReader sdr = ExecuteReader(cmd, CommandType.Text, query);
-                dtMenuItems.Load(sdr);
-                BuildTreeForthisUser(dtMenuItems);
-                this.uriIFrame.Attributes["src"] = "Default.aspx";
+                var sdr = ExecuteReader(cmd, CommandType.Text, query);
+                _dtMenuItems.Load(sdr);
+                BuildTreeForthisUser(_dtMenuItems);
+                uriIFrame.Attributes["src"] = "Default.aspx";
             }
         }
         public static MySqlDataReader ExecuteReader(MySqlCommand cmd, CommandType cmdType, string cmdText)
         {
-            MySqlConnection conn = new MySqlConnection(CONN_STRING);
+            MySqlConnection conn = new MySqlConnection(ConnString);
             try
             {
-                PrepareCommand(cmd, conn, (MySqlTransaction)null, cmdType, cmdText);
+                PrepareCommand(cmd, conn, null, cmdType, cmdText);
                 return cmd.ExecuteReader(CommandBehavior.CloseConnection);
             }
             catch
@@ -71,31 +66,28 @@ namespace DailyCollectionAndPayments
                 menuBar.Items.Add(new MenuItem(dr["previlage_name"].ToString(),
                         dr["previlage_id"].ToString()));
             }
-            menuBar.MenuItemClick += new MenuEventHandler(menuBar_MenuItemClick);
+            menuBar.MenuItemClick += menuBar_MenuItemClick;
             foreach (DataRow dr in dsmenuItems.Select("parent_previlage_id >" + 0))
             {               
                 MenuItem mnu = new MenuItem(dr["previlage_name"].ToString(),
-                              dr["previlage_id"].ToString()
-                              );
+                              dr["previlage_id"].ToString());
                 menuBar.FindItem(dr["parent_previlage_id"].ToString()).ChildItems.Add(mnu);
             }
         }
         protected void menuBar_MenuItemClick(object sender, MenuEventArgs e)
         {
-            String strRoleid = Session["RoleId"].ToString();
+            var strRoleid = Session["RoleId"].ToString();
             MySqlCommand cmd = new MySqlCommand(); 
             string query = " SELECT mp.previlage_id, previlage_name,parent_previlage_id, path  "
                     + "     FROM `finance`.m_previlage mp "
                     + "     JOIN `finance`.m_roleprevilage mr ON mp.previlage_id = mr.previlage_id "
                     + "     WHERE role_id = " + strRoleid + " ORDER BY order_by ASC; ";
             MySqlDataReader sdr = ExecuteReader(cmd, CommandType.Text, query);
-            dtMenuItems.Load(sdr);
-            foreach (DataRow dr in dtMenuItems.Rows)
+            _dtMenuItems.Load(sdr);
+            foreach (DataRow dr in _dtMenuItems.Rows)
             {
-                if (menuBar.SelectedItem.Text.Trim().ToString() == dr["previlage_name"].ToString())
-                {
-                    this.uriIFrame.Attributes["src"] = dr["path"].ToString();
-                }
+                if (menuBar.SelectedItem.Text.Trim() == dr["previlage_name"].ToString())
+                    uriIFrame.Attributes["src"] = dr["path"].ToString();
             }
         }
         protected void btnLogout_Click(object sender, EventArgs e)
